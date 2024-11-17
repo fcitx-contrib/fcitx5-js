09a4af000deb4857b9d740945e3b02e6107f9283
Qijia Liu <liumeo@pku.edu.cn>
2024-11-17 18:22:41 -0500

    move event_js.cpp out of fcitx5 source tree (#14)

---
+++ src/event_js.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <fcitx-utils/eventloopinterface.h>
+
+namespace fcitx {
+class JSEventLoop : public EventLoopInterface {
+  public:
+    JSEventLoop() = default;
+    ~JSEventLoop() override = default;
+    bool exec() override { return true; }
+    void exit() override {}
+    const char *implementation() const override { return "js"; }
+    void *nativeHandle() override { return nullptr; }
+
+    std::unique_ptr<EventSourceIO> addIOEvent(int fd, IOEventFlags flags,
+                                              IOCallback callback) override;
+    std::unique_ptr<EventSourceTime>
+    addTimeEvent(clockid_t clock, uint64_t usec, uint64_t accuracy,
+                 TimeCallback callback) override;
+    std::unique_ptr<EventSource> addExitEvent(EventCallback callback) override;
+    std::unique_ptr<EventSource> addDeferEvent(EventCallback callback) override;
+    std::unique_ptr<EventSource> addPostEvent(EventCallback callback) override;
+};
+} // namespace fcitx
