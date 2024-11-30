969337ad3c47d4b20437af0671db34b95762c751
Qijia Liu <liumeo@pku.edu.cn>
2024-11-30 14:07:30 -0500

    use en_dict.fscd from f5m

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -10,10 +10,15 @@ deps=(
 )
 
 EXTRACT_DIR=build/sysroot/usr
-mkdir -p $EXTRACT_DIR
+SPELL_DICT_DIR=$EXTRACT_DIR/share/fcitx5/spell
+mkdir -p $SPELL_DICT_DIR
 
 for dep in "${deps[@]}"; do
   file=$dep.tar.bz2
   [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-prebuilder/releases/download/js/$file
   tar xjvf cache/$file -C $EXTRACT_DIR
 done
+
+file=Fcitx5-arm64.tar.bz2
+[[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-macos/releases/download/latest/$file
+tar xjvf cache/$file -C $SPELL_DICT_DIR --strip-components=5 Fcitx5.app/Contents/share/fcitx5/spell/en_dict.fscd
