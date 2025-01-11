8a9ec2ff64a0311fab8d5d5bba6ebd562d79ba56
Qijia Liu <liumeo@pku.edu.cn>
2025-01-11 16:12:06 -0500

    regression: en_dict.fscd not packaged

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -10,15 +10,15 @@ deps=(
 )
 
 EXTRACT_DIR=build/sysroot/usr
-SPELL_DICT_DIR=$EXTRACT_DIR/share/fcitx5/spell
-mkdir -p $SPELL_DICT_DIR
+SPELL_DICT_DIR=build/destdir/usr/share/fcitx5/spell
+mkdir -p $EXTRACT_DIR $SPELL_DICT_DIR
 
 for dep in "${deps[@]}"; do
   file=$dep.tar.bz2
   [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-prebuilder/releases/download/js/$file
-  tar xjvf cache/$file -C $EXTRACT_DIR
+  tar xf cache/$file -C $EXTRACT_DIR
 done
 
 file=Fcitx5-arm64.tar.bz2
 [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-macos/releases/download/latest/$file
-tar xjvf cache/$file -C $SPELL_DICT_DIR --strip-components=5 Fcitx5.app/Contents/share/fcitx5/spell/en_dict.fscd
+tar xf cache/$file -C $SPELL_DICT_DIR --strip-components=5 Fcitx5.app/Contents/share/fcitx5/spell/en_dict.fscd
