f82ffa8b94760eb00b6e185b7e7c8301ed543d4f
Qijia Liu <liumeo@pku.edu.cn>
2025-02-22 22:51:31 -0500

    add e2e tests (#22)

---
+++ tests/test-generic.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '@playwright/test'
+import { init } from './util'
+
+test('keyboard-us', async ({ page }) => {
+  const addons: string[] = []
+  page.on('console', (msg) => {
+    const match = msg.text().match(/Loaded addon (\S+)/)
+    if (match) {
+      addons.push(match[1])
+    }
+  })
+
+  await init(page)
+  expect(addons.sort()).toEqual(['clipboard', 'imselector', 'keyboard', 'quickphrase', 'unicode', 'wasmfrontend', 'webpanel'])
+
+  const textarea = page.locator('textarea')
+  await textarea.click()
+  await page.keyboard.press('q')
+  await expect(textarea).toHaveValue('q')
+
+  const input = page.locator('input')
+  await input.click()
+  await page.keyboard.press('w')
+  await expect(input).toHaveValue('w')
+})
+
+test('keyboard-th', async ({ page }) => {
+  await init(page)
+
+  await page.evaluate(async () => {
+    window.fcitx.setInputMethods(['keyboard-th'])
+  })
+  const textarea = page.locator('textarea')
+  await textarea.click()
+  for (const key of 'l;ylfu') {
+    await page.keyboard.press(key)
+  }
+  await expect(textarea).toHaveValue('สวัสดี')
+})
