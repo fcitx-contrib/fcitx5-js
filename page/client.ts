7e2fee3aca882c9ce623fe4286609541d64f2f96
Qijia Liu <liumeo@pku.edu.cn>
2025-05-20 23:53:03 -0400

    render preedit underline

--- page/client.ts
+++ page/client.ts
@@ -7,7 +7,15 @@ let y = 0
 let preedit = ''
 let preeditIndex = 0
 
+// compared with macOS pinyin
+const UNDERLINE_OFFSET = 1
+const CANDIDATE_WINDOW_OFFSET = 6
+
 const textEncoder = new TextEncoder()
+
+function getFontSize(element: HTMLElement) {
+  return Number.parseFloat(getComputedStyle(element).fontSize)
+}
 
 export function placePanel(dx: number, dy: number, anchorTop: number, anchorLeft: number, dragging: boolean) {
   const input = getInputElement()
@@ -16,12 +24,13 @@ export function placePanel(dx: number, dy: number, anchorTop: number, anchorLeft
   }
   const rect = input.getBoundingClientRect()
   const { top, left, height } = getCaretCoordinates(input, input.selectionStart! - (window.fcitx.followCursor ? 0 : preeditIndex))
-  const h = height /* NaN if no line-height is set */ || Number(getComputedStyle(input).fontSize.slice(0, -'px'.length))
+  const h = height /* NaN if no line-height is set */ || getFontSize(input) + CANDIDATE_WINDOW_OFFSET
   const panel = <HTMLElement>document.querySelector('#fcitx-theme')
   const frame = panel.getBoundingClientRect()
   panel.style.opacity = '1'
   panel.style.position = 'absolute'
   panel.style.height = '0' // let mouse event pass through
+  panel.style.zIndex = '2147483647' // absolutely above preedit underline
   if (dragging) {
     x += dx
     y += dy
@@ -38,6 +47,59 @@ export function hidePanel() {
   x = y = 0
   const panel = <HTMLElement>document.querySelector('#fcitx-theme')
   panel.style.opacity = '0'
+}
+
+function clearPreeditUnderline() {
+  document.querySelectorAll('.fcitx-preedit-underline').forEach(div => div.remove())
+}
+
+function drawUnderline(top: number, left: number, width: number, color: string) {
+  const div = document.createElement('div')
+  div.className = 'fcitx-preedit-underline'
+  div.style.position = 'absolute'
+  div.style.top = `${top}px`
+  div.style.left = `${left}px`
+  div.style.height = '1px'
+  div.style.width = `${width}px`
+  div.style.backgroundColor = color
+  document.body.appendChild(div)
+}
+
+function getTextWidth(input: HTMLElement, text: string) {
+  const div = document.createElement('div')
+  const style = getComputedStyle(input)
+  div.style.position = 'absolute'
+  div.style.opacity = '0'
+  div.style.font = style.font
+  div.textContent = text
+  document.body.append(div)
+  const { width } = div.getBoundingClientRect()
+  div.remove()
+  return width
+}
+
+function drawPreeditUnderline(input: HTMLElement, start: number) {
+  const box = input.getBoundingClientRect()
+  const color = getComputedStyle(input).color
+  const fontSize = getFontSize(input)
+  const { top: startTop, left: startLeft } = getCaretCoordinates(input, start)
+  const { top: endTop, left: endLeft } = getCaretCoordinates(input, start + preedit.length)
+  let lastLeft = startLeft
+  for (let i = 1, rowLeft = startLeft, rowTop = startTop; i <= preedit.length && rowTop < endTop; ++i) {
+    const { top, left } = getCaretCoordinates(input, start + i)
+    if (top !== rowTop) {
+      // getCaretCoordinates can't tell the position of the end of previous line,
+      // because it's equivalent to the start of next line, which is the actual place
+      // that new character is written. So we need to calculate width of the last character.
+      drawUnderline(box.top + rowTop + fontSize + UNDERLINE_OFFSET, box.left + rowLeft, lastLeft - rowLeft + getTextWidth(input, preedit[i - 1]), color)
+      rowTop = top
+      rowLeft = lastLeft
+    }
+    lastLeft = left
+  }
+  if (lastLeft !== endLeft) {
+    drawUnderline(box.top + endTop + fontSize + UNDERLINE_OFFSET, box.left + lastLeft, endLeft - lastLeft, color)
+  }
 }
 
 function changeInput(commitText: string, preeditText: string, index: number) {
@@ -64,15 +126,20 @@ ____ commit pre|edit ____
 
   const start = input.selectionStart! - preeditIndex
   const end = preedit ? start + preedit.length : input.selectionEnd!
+  const newStart = start + commitText.length
   input.value = input.value.slice(0, start) + commitText + preeditText + input.value.slice(end)
   // This may be triggered by user clicking panel. Focus to ensure setting selectionEnd works.
   input.focus()
-  input.selectionStart = input.selectionEnd = start + commitText.length + i
+  input.selectionStart = input.selectionEnd = newStart + i
   // For vue-based input, this is needed to synchronize state.
   input.dispatchEvent(new Event('change'))
   preedit = preeditText
   preeditIndex = i
   setSpellCheck(!preedit)
+  clearPreeditUnderline()
+  if (preedit) {
+    drawPreeditUnderline(input, newStart)
+  }
 }
 
 export function setPreedit(text: string, index: number) {
@@ -86,4 +153,5 @@ export function commit(text: string) {
 export function resetPreedit() {
   preedit = ''
   preeditIndex = 0
+  clearPreeditUnderline()
 }
