132111839553960d9591fec854c2cb57d5bedd60
Qijia Liu <liumeo@pku.edu.cn>
2025-06-22 15:33:37 -0400

    simulate left/right/home/end (#50)

--- tests/test-simulate.mobile.spec.ts
+++ tests/test-simulate.mobile.spec.ts
@@ -1,5 +1,79 @@
+import type { Locator, Page } from '@playwright/test'
 import { expect, test } from '@playwright/test'
 import { expectKeyboardShown, init, tapKeyboard } from './util'
+
+function openEditor(page: Page) {
+  return page.locator('.fcitx-keyboard-toolbar-button:nth-child(3)').tap()
+}
+
+function getSelection(locator: Locator): Promise<[number, number]> {
+  return locator.evaluate((el: HTMLTextAreaElement) => [el.selectionStart, el.selectionEnd])
+}
+
+test('Printable', async ({ page }) => {
+  await init(page)
+  const textarea = page.locator('textarea')
+  await textarea.tap()
+  await expectKeyboardShown(page)
+  await textarea.evaluate((el: HTMLTextAreaElement) => {
+    el.value = 'ad'
+    el.selectionStart = el.selectionEnd = 1
+  })
+
+  await tapKeyboard(page, 'b!')
+  await tapKeyboard(page, 'c;')
+  await expect(textarea, 'selectionStart is not reset to the end').toHaveValue('abcd')
+})
+
+test('ArrowLeft and ArrowRight', async ({ page }) => {
+  await init(page)
+
+  const textarea = page.locator('textarea')
+  await textarea.tap()
+  await expectKeyboardShown(page)
+  await textarea.evaluate((el: HTMLTextAreaElement) => {
+    el.value = '左🦨右'
+    el.selectionStart = el.selectionEnd = 3
+  })
+  await openEditor(page)
+
+  const left = page.locator('.fcitx-keyboard-editor-button-container:nth-child(1)')
+  const right = page.locator('.fcitx-keyboard-editor-button-container:nth-child(3)')
+  await left.tap()
+  expect(await textarea.evaluate((el: HTMLTextAreaElement) => [el.selectionStart, el.selectionEnd])).toEqual([1, 1])
+
+  await right.tap()
+  expect(await getSelection(textarea)).toEqual([3, 3])
+  await right.tap()
+  expect(await getSelection(textarea)).toEqual([4, 4])
+})
+
+test('Home and End', async ({ page }) => {
+  await init(page)
+  const textarea = page.locator('textarea')
+  await textarea.tap()
+  await expectKeyboardShown(page)
+  await textarea.evaluate((el: HTMLTextAreaElement) => {
+    el.value = 'ab\ncd'
+    el.selectionStart = el.selectionEnd = 1
+  })
+  await openEditor(page)
+
+  const home = page.locator('.fcitx-keyboard-editor-button-container:nth-child(10)')
+  const end = page.locator('.fcitx-keyboard-editor-button-container:nth-child(11)')
+  await end.tap()
+  expect(await getSelection(textarea)).toEqual([2, 2])
+  await home.tap()
+  expect(await getSelection(textarea)).toEqual([0, 0])
+
+  await textarea.evaluate((el: HTMLTextAreaElement) => {
+    el.selectionStart = el.selectionEnd = 4
+  })
+  await end.tap()
+  expect(await getSelection(textarea)).toEqual([5, 5])
+  await home.tap()
+  expect(await getSelection(textarea)).toEqual([3, 3])
+})
 
 test('Backspace', async ({ page }) => {
   await init(page)
