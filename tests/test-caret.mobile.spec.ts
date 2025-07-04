c7f8e379a8c0736cc28119d1350b6108f98f15d2
Qijia Liu <liumeo@pku.edu.cn>
2025-07-04 09:06:51 -0400

    hide mobile caret outside of input element (#61)

--- tests/test-caret.mobile.spec.ts
+++ tests/test-caret.mobile.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test'
-import { expectKeyboardShown, getBox, init, tapKeyboard } from './util'
+import { browserName, expectKeyboardShown, getBox, init, tapKeyboard } from './util'
 
 test('Focus, blink and blur', async ({ page }) => {
   await init(page)
@@ -55,6 +55,92 @@ test('Touch', async ({ page }) => {
     const { x: x1 } = await getBox(caret)
     // Tapping on center should effectively change caret position
     if (x1 > x0) {
+      break
+    }
+  }
+})
+
+test('Caret follows page scroll', async ({ page }) => {
+  await init(page)
+
+  const container = page.locator('.container')
+  await container.evaluate((el) => {
+    el.style.width = '100px'
+    el.style.height = '100px'
+    el.style.overflow = 'auto'
+  })
+  const textarea = page.locator('textarea')
+  await textarea.evaluate((el) => {
+    el.style.width = '120px'
+    el.style.height = '120px'
+  })
+  await textarea.tap()
+  const caret = page.locator('.fcitx-mobile-caret')
+  const box = await getBox(caret)
+
+  await container.evaluate(el => el.scrollBy(10, 10))
+  let newBox
+  while (true) {
+    newBox = await getBox(caret)
+    if (newBox.x < box.x) {
+      break
+    }
+  }
+  expect(newBox.x).toBeCloseTo(box.x - 10, 0.1)
+  expect(newBox.y).toBeCloseTo(box.y - 10, 0.1)
+})
+
+test('Caret follows input horizontal scroll', async ({ page }) => {
+  test.skip(browserName(page) === 'webkit') // Safari's input can't scroll horizontally.
+  await init(page)
+
+  const input = page.locator('input')
+  await input.evaluate(el => el.style.width = '20px')
+
+  await input.tap()
+  await input.evaluate((el: HTMLInputElement) => {
+    el.value = 'aaaaaa'
+    el.selectionStart = el.selectionEnd = 0
+  })
+  const caret = page.locator('.fcitx-mobile-caret')
+  await expect(caret).toHaveCount(1)
+
+  await input.evaluate(el => el.scrollBy(5, 0))
+  await expect(caret).toHaveCount(0)
+})
+
+test('Caret follows input vertical scroll', async ({ page }) => {
+  await init(page)
+
+  const textarea = page.locator('textarea')
+  await textarea.tap()
+  await textarea.evaluate((el: HTMLTextAreaElement) => {
+    el.value = '\n\n\na'
+    el.selectionStart = el.selectionEnd = 0
+  })
+  const caret = page.locator('.fcitx-mobile-caret')
+  await expect(caret).toHaveCount(1)
+  await textarea.evaluate(el => el.scrollBy(0, 50))
+  await expect(caret).toHaveCount(0)
+})
+
+test('Caret follows input resize', async ({ page }) => {
+  await init(page)
+
+  const textarea = page.locator('textarea')
+  await textarea.tap()
+  await textarea.evaluate((el: HTMLTextAreaElement) => {
+    el.value = 'aaaaaaaaaa'
+    el.selectionStart = el.selectionEnd = el.value.length
+  })
+  const caret = page.locator('.fcitx-mobile-caret')
+  const box = await getBox(caret)
+
+  await textarea.evaluate(el => el.style.width = '50px')
+  let newBox
+  while (true) {
+    newBox = await getBox(caret)
+    if (newBox.y > box.y) {
       break
     }
   }
