d341c6f04e71b39e9438c0facfe4a06ac43627cc
Qijia Liu <liumeo@pku.edu.cn>
2025-10-05 13:37:54 -0400

    fix install.sh dependencies (#69)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
+set -e
+
 export DESTDIR=build/destdir
-for dir in chromepanel fcitx5 wasmfrontend wasmnotifications webkeyboard webpanel; do
+for dir in fcitx5/src/im fcitx5/src/lib fcitx5/src/modules fcitx5/po chromepanel wasmfrontend wasmnotifications webkeyboard webpanel; do
   cmake --install build/$dir
 done
 touch build/dummy.cpp
